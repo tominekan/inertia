734140816b6337dcca79d22be4aa04a066a6c2e7
a2k6 <adrianpanezic@comcast.net>
2022-12-02 00:40:09 -0800

    More debugging and added framework and comments for future animations

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+let detectorUp = false, detectorDown = false;
+//Add in a function that detects when the user is above or below and adjusts
+//the animation accordingly(makes the elements come in from the right direction)
+//Consider making a function that will change the transform(transformX to transformY or just to change values)
+//of the hidden class so that there aren't too many css classes being added at once
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('show');
+            console.warn(window.innerHeight);
+            console.warn(window.innerWidth);
         }else {
             entry.target.classList.remove('show');
         }
